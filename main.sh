5b9f8118aa0259689d53827a2ea7e651b3db360d
josedhontas <jose.sales@dcomp.ufs.br>
2024-04-27 16:40:08 -0300

    teste

--- main.sh
+++ main.sh
@@ -8,3 +8,8 @@ sudo apt-get install transmission-cli -y
 sudo apt-get install ffmpeg -y
 
 echo "Instalação concluída."
+
+
+transmission-cli "magnet:?xt=urn:btih:B7399C4239C8A0540BC70BD065575C6B7D133AB3&dn=Trilogia%20O%20Senhor%20dos%20An%c3%a9is%20%282001%20-%202003%29%20BluRay%20720p%20Dual%20%c3%81udio%205.1&tr=udp%3a%2f%2ftracker.openbittorrent.com%3a80%2fannounce&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce"
+
+
