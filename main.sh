67ebe3b62d2f510d9e873a8f6f6707f880c71c51
josedhontas <jose.sales@dcomp.ufs.br>
2024-04-27 16:17:57 -0300

    first commit

---
+++ main.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Instalar Transmission
+sudo apt-get update
+sudo apt-get install transmission-cli -y
+
+# Instalar FFmpeg
+sudo apt-get install ffmpeg -y
+
+echo "Instalação concluída."
