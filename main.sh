adf91404a4370b1d272dbd7f005fd4ea9faa1d9f
José Dhonatas Alves Sales <75862011+josedhontas@users.noreply.github.com>
2024-04-27 17:15:26 -0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -9,8 +9,9 @@ cd ..
 
 ##echo "Instalação concluída."
 
+## senhor dos aneis 
 
-##transmission-cli "magnet:?xt=urn:btih:B7399C4239C8A0540BC70BD065575C6B7D133AB3&dn=Trilogia%20O%20Senhor%20dos%20An%c3%a9is%20%282001%20-%202003%29%20BluRay%20720p%20Dual%20%c3%81udio%205.1&tr=udp%3a%2f%2ftracker.openbittorrent.com%3a80%2fannounce&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce"
+transmission-cli "magnet:?xt=urn:btih:B7399C4239C8A0540BC70BD065575C6B7D133AB3&dn=Trilogia%20O%20Senhor%20dos%20An%c3%a9is%20%282001%20-%202003%29%20BluRay%20720p%20Dual%20%c3%81udio%205.1&tr=udp%3a%2f%2ftracker.openbittorrent.com%3a80%2fannounce&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce"
 
 ## Donie darko
 
